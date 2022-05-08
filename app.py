89629fca2df1028de00efa0a7ad299e29f5af4a8
“Frederic-vW” <fvw.github@gmail.com>
2022-05-09 08:20:28 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Flask app
+
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", now=datetime.now())
+
+if __name__ == "__main__":
+    app.run()
